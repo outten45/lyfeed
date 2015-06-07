785c7189771e8e98e1287e48a60778ce83c89f27
Richard Outten <outtenr@gmail.com>
2015-06-07 15:33:50 -0400

    Adding web server
    Add basic router and server

--- src/lyfeed/cmd/lyfeed/main.go
+++ src/lyfeed/cmd/lyfeed/main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"fmt"
 	"lyfeed/importer"
+	"net/http"
+
+	"github.com/codegangsta/negroni"
+	"github.com/julienschmidt/httprouter"
 )
+
+func myHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	fmt.Fprintf(w, "Hello %v\n", ps.ByName("id"))
+}
 
 func main() {
 	fmt.Println("Made it!")
 	importer.Run()
 	fmt.Println("----------")
+
+	mux := httprouter.New()
+	mux.GET("/page/:id", myHandler)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":3000")
+
 }
