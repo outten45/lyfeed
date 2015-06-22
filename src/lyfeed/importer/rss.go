af5438247a494029a73d059cdd7ee42cd754a90f
Richard Outten <outtenr@gmail.com>
2015-06-21 22:15:27 -0400

    Create custom rss handler
    Add custom rss handler that can be passed the Context for things like
    the database and logging.

--- src/lyfeed/importer/rss.go
+++ src/lyfeed/importer/rss.go
@@ -13,12 +13,13 @@ import (
 
 func Run(c *lyfeed.Context, feedURLs []string) {
 	for _, f := range feedURLs {
-		go PollFeed(f, 5, nil)
+		go PollFeed(c, f, 5, nil)
 	}
 }
 
-func PollFeed(uri string, timeout int, cr xmlx.CharsetFunc) {
-	feed := rss.New(timeout, true, chanHandler, itemHandler)
+func PollFeed(c *lyfeed.Context, uri string, timeout int, cr xmlx.CharsetFunc) {
+	h := &RssHandler{Context: c}
+	feed := rss.NewWithHandlers(timeout, true, h, h)
 
 	for {
 		if err := feed.Fetch(uri, cr); err != nil {
@@ -30,12 +31,16 @@ func PollFeed(uri string, timeout int, cr xmlx.CharsetFunc) {
 	}
 }
 
-func chanHandler(feed *rss.Feed, newchannels []*rss.Channel) {
+type RssHandler struct {
+	Context *lyfeed.Context
+}
+
+func (mh *RssHandler) ProcessChannels(feed *rss.Feed, newchannels []*rss.Channel) {
 	// fmt.Printf("----------------------------------------\nchannel: \n%+v\n\n\n", newchannels[0])
 	fmt.Printf("%d new channel(s) in %s\n", len(newchannels), feed.Url)
 }
 
-func itemHandler(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
+func (mn *RssHandler) ProcessItems(feed *rss.Feed, ch *rss.Channel, newitems []*rss.Item) {
 	fmt.Printf("%d new item(s) in %s\n", len(newitems), feed.Url)
 	fmt.Printf("---------------------------------------------------------\n")
 	fmt.Printf("%+v\n", newitems[0])
